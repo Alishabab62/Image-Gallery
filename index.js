0abff4623843357092b157749e80040802f1cd8b
AliShabab62 <Alishabab62@gmail.com>
2022-11-29 16:05:21 +0530

    Done

--- index.js
+++ index.js
@@ -1,75 +1,65 @@
-const itemContainer=document.getElementById('item-container');
-const addBtn=document.getElementById('btn');
-const cat=document.getElementById('select');
-const link=document.getElementById('text');
-const invalid=document.getElementById('para');
+const itemContainer = document.getElementById("item-container");
+const addBtn = document.getElementById("btn");
+const cat = document.getElementById("select");
+const link = document.getElementById("text");
+const invalid = document.getElementById("para");
 
-
-let itemArr=[];
-addBtn.addEventListener('click',(e)=>{
-    if(link.value==""){
-        invalid.innerText="No Link present";
-        setTimeout(()=>{
-            invalid.innerText="";
-        },1000);
-    }
-    else{
-    const itemDiv=document.createElement('div');
-    const imgTag=document.createElement('img');
-    const button=document.createElement('button');
+let itemArr = [];
+addBtn.addEventListener("click", (e) => {
+  if (link.value == "") {
+    invalid.innerText = "No Link present";
+    setTimeout(() => {
+      invalid.innerText = "";
+    }, 1000);
+  } else {
+    const itemDiv = document.createElement("div");
+    const imgTag = document.createElement("img");
+    const button = document.createElement("button");
     itemDiv.append(imgTag);
     itemDiv.append(button);
-    imgTag.src=link.value;
-    button.classList="delete-btn";
-    itemDiv.classList=`store-item ${cat.value}`;
-    button.innerText ="Delete";
+    imgTag.src = link.value;
+    button.classList = "delete-btn";
+    itemDiv.classList = `store-item ${cat.value}`;
+    button.innerText = "Delete";
     itemArr.push({
-        "url" : link.value,
-        "type" : cat.value
-    })
-    console.log(itemArr)
+      url: link.value,
+      type: cat.value,
+    });
+    console.log(itemArr);
     itemContainer.appendChild(itemDiv);
-    link.value="";
-    }
+    link.value = "";
+  }
 });
 
 //For Delete
 
-itemContainer.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('delete-btn')){
-        let imgSrc=e.target.previousElementSibling.src;
-        console.log(imgSrc)
-        let delteIndex=itemArr.findIndex(item=> item.url==imgSrc);
-        itemArr.splice(delteIndex,1);
-        e.target.parentNode.remove();
-    }
-    console.log(itemArr)
+itemContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-btn")) {
+    let imgSrc = e.target.previousElementSibling.src;
+    console.log(imgSrc);
+    let delteIndex = itemArr.findIndex((item) => item.url == imgSrc);
+    itemArr.splice(delteIndex, 1);
+    e.target.parentNode.remove();
+  }
+  console.log(itemArr);
 });
 
-
-
-function filterItem(e){
-    e.preventDefault();
-    const storeImages=document.querySelectorAll('.store-item');
-    const filter=e.target.dataset.filter;
-    storeImages.forEach((item)=>{
-        if(filter == 'all'){
-            item.style.display='block';
-        }
-        else if(item.classList.contains(filter)){
-            item.style.display='block';
-        }else{
-            item.style.display='none';
-        }
-    })
-
+function filterItem(e) {
+  e.preventDefault();
+  const storeImages = document.querySelectorAll(".store-item");
+  const filter = e.target.dataset.filter;
+  storeImages.forEach((item) => {
+    if (filter == "all") {
+      item.style.display = "block";
+    } else if (item.classList.contains(filter)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
 
-
-const buttons=document.querySelectorAll('.btn');
-buttons.forEach((filterButton)=>{
-    filterButton.addEventListener('click',filterItem);
-})
-
-
-
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((filterButton) => {
+  filterButton.addEventListener("click", filterItem);
+});
